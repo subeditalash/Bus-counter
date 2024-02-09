da6c43ddda0081de00eec6ade8c842876c87b36c
Talaash Subedi <93320625+subeditalash@users.noreply.github.com>
2024-02-09 16:47:12 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './Bus'
+import Bus from './Bus.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Bus/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
